6b4e26f80f1ff06e2c4e33812bdf9d6bf4562388
d6stiny <99969901+d6stiny@users.noreply.github.com>
2023-02-08 20:39:16 +0000

    yas

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@ import Head from "next/head";
 import Image from "next/image";
 import { Nunito } from "@next/font/google";
 import Link from "next/link";
+import clsx from "clsx";
+import type { PropsWithChildren } from "react";
+import { SiMastodon } from "react-icons/si";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -15,7 +18,7 @@ export default function Home() {
       <Head>
         <title>d6stiny</title>
       </Head>
-      <main className="mx-auto grid max-w-3xl grid-cols-6 gap-6 px-6 pb-40 pt-16">
+      <main className="font-nunito mx-auto grid max-w-3xl grid-cols-6 gap-6 px-6 pb-40 pt-16">
         <div className="p-200 col-span-4 flex items-center justify-center overflow-hidden rounded-2xl border-pink-500 bg-pink-500/20 backdrop-blur-2xl md:col-span-4 md:h-52">
           <div className="flex flex-col items-center space-y-4 py-8 px-6 md:flex-row md:space-y-0 md:space-x-4">
             <Image
@@ -47,7 +50,32 @@ export default function Home() {
             </div>
           </div>
         </div>
+        <Card className="col-span-2 h-full">
+          <Link
+            href="https://mstdn.party/@d6stiny"
+            target="_blank"
+            rel="noopener noreferrer me"
+            className={clsx(
+              "flex h-full items-center justify-center rounded-2xl bg-[#6364FF] text-4xl text-white",
+              cardStyles
+            )}
+          >
+            <span className="sr-only">Twitter</span>
+            <span className="transform-gpu transition group-hover:-rotate-[10deg] group-hover:scale-[1.3]">
+              <SiMastodon />
+            </span>
+          </Link>
+        </Card>
       </main>
     </>
   );
 }
+
+const cardStyles =
+  "transform-gpu transition-all duration-500 will-change-[outline,_transform] group-hover:scale-95 active:scale-100";
+const Card = ({
+  children,
+  ...props
+}: PropsWithChildren<{ className?: string }>) => {
+  return <div className={clsx("group", props.className)}>{children}</div>;
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -11,5 +15,25 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"),
+    {
+      handler: (tw) => {
+        tw.matchUtilities(
+          {
+            "text-glow": (value) => ({
+              "text-shadow": `0 0 10px ${value}, 0 0 150px ${value}`,
+            }),
+            glow: (value) => ({
+              filter: `drop-shadow(0px 0px 7px ${value})`,
+            }),
+          },
+          {
+            values: flattenColorPalette(tw.theme("colors")),
+            type: "color",
+          }
+        );
+      },
+    },
+  ],
 };
