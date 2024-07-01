44187ae14ef92d7a65e8565755cd9f0df54237b7
d6stiny <99969901+d6stiny@users.noreply.github.com>
2024-07-01 15:44:11 +0100

    feat: Add new internship report and update page layout

--- src/app/epm/internship/page.tsx
+++ src/app/epm/internship/page.tsx
@@ -10,6 +10,13 @@ export default function Internship() {
         src="/relatório_estágio.pdf"
         className="mt-4 aspect-square w-full"
       />
+      <h1 className="mt-6 text-center text-4xl">
+        Relatório de estágio (3º ano)
+      </h1>
+      <iframe
+        src="/2relatório_estágio.pdf"
+        className="mt-4 aspect-square w-full"
+      />
     </>
   );
 }
